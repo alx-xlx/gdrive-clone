b5774b007a253c2973611ba848516214b046097c
r4nchy <42188371+alx-xlx@users.noreply.github.com>
2020-08-26 18:18:19 +0530

    gcopy:

--- gcopy.sh
+++ gcopy.sh
@@ -1,7 +1,11 @@
-# DEFAULTDESTINATION='YOUR_DESTINATION_TEAMDRIVE_ID'
-DEFAULTDESTINATION='0AD1zzAs7falwUk9PVA'
-# FILEPATH=/PATH/TO/YOUR/SA/ACCOUNTS/1.json
-REPOPATH=/home/ubuntu/gclone
+# DEFAULTDESTINATION='YOUR_DESTINATION_TEAMDRIVE_ID OR My Drive Folder ID having Write Permission'
+# REPOPATH=/PATH/TO/YOUR/REPOSITORY
+
+DEFAULTDESTINATION='0AD1zzAs7falwUk9PVA'        
+REPOPATH=/home/ubuntu/gdrive-clone
+
+# ----------------------------------------------------------------------
+
 FILEPATH=${REPOPATH}/accounts/1.json
 
 CHECK_MARK="\033[0;32m\xE2\x9C\x94\033[0m"
@@ -13,7 +17,7 @@ echo "                   `date`
 |  _  |  _  |   _|  |  |  |  -__|______|  __|  |  _  |     |  -__|
 |___  |_____|__| |__|\___/|_____|      |____|__|_____|__|__|_____|
 |_____|                                                           
-               https://github.com/alx-xlx/gclone                   "
+              https://github.com/alx-xlx/gdrive-clone               "
 
 echo -n "Checking for Accounts Folder"
 sleep 2
@@ -23,7 +27,7 @@ else
     echo -e "\\r${CROSS} Folder is not Present"
     exit 0
 fi
-echo -en "[gc]\ntype = drive\nscope = drive\nservice_account_file = /home/ubuntu/gclone/accounts/1.json\nservice_account_file_path = /home/ubuntu/gclone/accounts/" >> rc.conf
+echo -en "[gc]\ntype = drive\nscope = drive\nservice_account_file = ${REPOPATH}/accounts/1.json\nservice_account_file_path = ${REPOPATH}/accounts/" >> ${REPOPATH}/rc.conf
 sleep 2
 echo "Reply with Source Folder Link"
 read SOURCE
@@ -41,8 +45,8 @@ echo "DESTINATION = https://drive.google.com/drive/u/0/folders/$DESTINATION"
 sleep 5
 echo -e "\e[1A\e[K\e[1A\e[K\e[1A\e[0K${CHECK_MARK} Verified"
 echo -n `echo ${CHECK_MARK} $SOURCE | sed 's/.*folders\///'`
-echo  "  ---->  $DST"
-${REPOPATH}/gclone copy gc:{`echo $SOURCE | sed 's/.*folders\///'`} gc:{$DESTINATION} --config=rc.conf --transfers 50 -v --stats-one-line -P --stats=15s --ignore-existing --drive-server-side-across-configs --drive-chunk-size 128M --drive-acknowledge-abuse --drive-keep-revision-forever
+echo  "  ---->  $DESTINATION"
+${REPOPATH}/gclone copy gc:{`echo $SOURCE | sed 's/.*folders\///'`} gc:{$DESTINATION} --config=${REPOPATH}/rc.conf --transfers 50 -v --stats-one-line -P --stats=15s --ignore-existing --drive-server-side-across-configs --drive-chunk-size 128M --drive-acknowledge-abuse --drive-keep-revision-forever
 sleep 2
 rm rc.conf
 exit
